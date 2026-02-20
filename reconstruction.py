f6ce0ddeb45b4f522f22fe6a9ed58311ac09a43c
RACHA114 <127985239+RACHA114@users.noreply.github.com>
2026-02-20 15:53:16 -0500

    Refactor readInput function for better parameter handling
    Updated input reading function to accept directory and filename parameters. Adjusted sinogram reading and added comments for clarity.

--- reconstruction.py
+++ reconstruction.py
@@ -16,16 +16,17 @@ import CTfiltre as CTfilter
 
 ## créer l'ensemble de données d'entrée à partir des fichiers
 def readInput(dir: str = geo.dataDir,
-              filename):
+              filename: str = geo.sinogramFile,
+              ):
     # lire les angles
-    [nbprj, angles] = util.readAngles(geo.dataDir+geo.anglesFile)
+    [nbprj, angles] = util.readAngles(dir+geo.anglesFile)
 
-    print("nbprj:",nbprj)
-    print("angles min and max (rad):")
-    print("["+str(np.min(angles))+", "+str(np.max(angles))+"]")
+    # print("nbprj:",nbprj)
+    # print("angles min and max (rad):")
+    # print("["+str(np.min(angles))+", "+str(np.max(angles))+"]")
 
     # lire le sinogramme
-    [nbprj2, nbpix2, sinogram] = util.readSinogram(geo.dataDir+filename)
+    [nbprj2, nbpix2, sinogram] = util.readSinogram(dir+filename)
 
     if nbprj != nbprj2:
         print("angles file and sinogram file conflict, aborting!")
@@ -38,7 +39,7 @@ def readInput(dir: str = geo.dataDir,
     return [nbprj, angles, sinogram]
 
 
-## reconstruire une image TDM en mode retroprojection
+## reconstruire une image TDM en mode rétroprojection
 def laminogram():
     
     [nbprj, angles, sinogram] = readInput()
@@ -51,13 +52,22 @@ def laminogram():
     # pour chaque voxel, trouver la contribution du signal reçu
     for j in range(geo.nbvox): # colonnes de l'image
         print("working on image column: "+str(j+1)+"/"+str(geo.nbvox))
+        
         for i in range(geo.nbvox): # lignes de l'image
+
             for a in range(len(angles)):
                 #votre code ici...
+                #le défi est simplement géométrique;
+                #pour chaque voxel, trouver la position par rapport au centre de la
+                #grille de reconstruction et déterminer la position d'arrivée
+                #sur le détecteur d'un rayon partant de ce point et atteignant
+                #le détecteur avec un angle de 90 degrés. Vous pouvez utiliser
+                #le pixel le plus proche ou interpoler linéairement...Rappel, le centre
+                #du détecteur est toujours aligné avec le centre de la grille de
+                #reconstruction peu importe l'angle.
                 pass
-                
-    util.saveImage(image, "lam")
 
+    util.saveImage(image, "lam")
 
 ## reconstruire une image TDM en mode retroprojection filtrée
 def backproject():
@@ -76,13 +86,16 @@ def backproject():
     # pour chaque voxel, trouver la contribution du signal reçu
     for j in range(geo.nbvox): # colonnes de l'image
         print("working on image column: "+str(j+1)+"/"+str(geo.nbvox))
+
         for i in range(geo.nbvox): # lignes de l'image
+
             for a in range(len(angles)):
                 #votre code ici
+                #pas mal la même chose que prédédemment
+                #mais avec un sinogramme qui aura été préalablement filtré
                 pass
-    
+            
     util.saveImage(image, "fbp")
-
 
 ## reconstruire une image TDM en mode retroprojection
 def reconFourierSlice():
@@ -99,15 +112,21 @@ def reconFourierSlice():
     #image reconstruite
     image = np.zeros((geo.nbvox, geo.nbvox))
     #votre code ici
+    #ici le défi est de remplir l'IMAGE avec des TF des projections (1D)
+    #au bon angle.
+    #La grille de recon est cartésienne mais le remplissage est cylindrique,
+    #ce qui fait qu'il y aura un bon échantillonnage de IMAGE
+    #au centre et moins bon en périphérie. Un TF inverse de IMAGE vous
+    #donnera l'image recherchée.
+
    
     
     util.saveImage(image, "fft")
 
 
 ## main ##
-start_time = time.time()
-laminogram()
-#backproject()
-#reconFourierSlice()
-print("--- %s seconds ---" % (time.time() - start_time))
-
+# start_time = time.time()
+# laminogram()
+# backproject()
+# #reconFourierSlice()
+# print("--- %s seconds ---" % (time.time() - start_time))
